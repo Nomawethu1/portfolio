888d0343cbce4c0626549da49925069c015efece
Nomawethu <nomawethu.Khoza@dadevs.co.za>
2022-11-21 11:25:48 +0200

    portfolio part

--- src/components/portfolio/portfolio.jsx
+++ src/components/portfolio/portfolio.jsx
@@ -1,9 +1,50 @@
 import React from 'react'
 import './portfolio.css'
+import Img1 from '../../assets/track.jpeg'
+import Img2 from '../../assets/images.jpeg'
+import Img3 from '../../assets/app.jpeg'
+import Img4 from '../../assets/emp.jpeg'
+
 
 const portfolio = () => {
   return (
-    <section id="portfolio">portfolio</section>
+    <section id="portfolio">
+      <h5>My Recent Work</h5>
+      <h2>Profile</h2>
+      <div className="container portfolio__container">
+        <article className='portfolio__item'>
+          <div className="portfolio__itaem-img">
+            <img src={Img1} alt="" />
+            <h3>This is a portfolio item tittle</h3>
+            <a href="https://github.com/Nomawethu1/apartmentfinder" className='btn btn-primary'>GitHub</a>
+          </div>
+        </article>
+
+        <article className='portfolio__item'>
+          <div className="portfolio__itaem-img">
+            <img src={Img2} alt="" />
+            <h3>This is a portfolio item tittle</h3>
+            <a href="https://github.com/Nomawethu1/apartmentfinder" className='btn btn-primary'>GitHub</a>
+          </div>
+        </article>
+
+        <article className='portfolio__item'>
+          <div className="portfolio__itaem-img">
+            <img src={Img3} alt="" />
+            <h3>This is a portfolio item tittle</h3>
+            <a href="https://github.com/Nomawethu1/apartmentfinder" className='btn btn-primary'>GitHub</a>
+          </div>
+        </article>
+
+        <article className='portfolio__item'>
+          <div className="portfolio__itaem-img">
+            <img src={Img4} alt="" />
+            <h3>This is a portfolio item tittle</h3>
+            <a href="https://github.com/Nomawethu1/apartmentfinder" className='btn btn-primary'>GitHub</a>
+          </div>
+        </article>
+      </div>
+    </section>
   )
 }
 
--- src/components/services/services.jsx
+++ src/components/services/services.jsx
@@ -1,9 +1,118 @@
 import React from 'react'
 import './services.css'
+import{BsCheck2} from 'react-icons/bs'
 
 const services = () => {
   return (
-    <section id="services">services</section>
+    <section id="services">
+      <h5>What I offer</h5>
+      <h2>Services</h2>
+      <div className="container services__container">
+        <article className="service">
+          <div className="service_head">
+            <h3>UI/UX design</h3>
+          </div>
+          <ul className="service_list">
+            <li>
+              <BsCheck2 className='service__list-icon'/>
+              <p>Lorem ipsum, dolor sit amet consectetur elit.</p>
+            </li>
+            <li>
+              <BsCheck2 className='service__list-icon'/>
+              <p>Lorem ipsum, dolor sit amet elit.</p>
+            </li>
+            <li>
+              <BsCheck2 className='service__list-icon'/>
+              <p>Lorem ipsum, dolor sit amet consectetur elit.</p>
+            </li>
+            <li>
+              <BsCheck2 className='service__list-icon'/>
+              <p>Lorem ipsum, dolor sit amet consectetur elit.</p>
+            </li>
+            <li>
+              <BsCheck2 className='service__list-icon'/>
+              <p>Lorem ipsum, dolor sit amet consectetur elit.</p>
+            </li>
+            <li>
+              <BsCheck2 className='service__list-icon'/>
+              <p>Lorem ipsum, dolor sit amet consectetur elit.</p>
+            </li>
+          </ul>
+        </article>
+        {/* end of first card */}
+
+        <article className="service">
+          <div className="service_head">
+            <h3>Web Development</h3>
+          </div>
+          <ul className="service_list">
+            <li>
+              <BsCheck2 className='service__list-icon'/>
+              <p>Lorem ipsum, dolor sit amet consectetur elit.</p>
+            </li>
+            <li>
+              <BsCheck2 className='service__list-icon'/>
+              <p>Lorem ipsum, dolor sit amet consectetur elit.</p>
+            </li>
+            <li>
+              <BsCheck2 className='service__list-icon'/>
+              <p>Lorem ipsum, dolor sit amet consectetur elit.</p>
+            </li>
+            <li>
+              <BsCheck2 className='service__list-icon'/>
+              <p>Lorem ipsum, dolor sit amet consectetur elit.</p>
+            </li>
+            <li>
+              <BsCheck2 className='service__list-icon'/>
+              <p>Lorem ipsum, dolor sit amet consectetur elit.</p>
+            </li>
+            <li>
+              <BsCheck2 className='service__list-icon'/>
+              <p>Lorem ipsum, dolor sit amet consectetur elit.</p>
+            </li>
+
+            <li>
+              <BsCheck2 className='service__list-icon'/>
+              <p>Lorem ipsum, dolor sit amet consectetur elit.</p>
+            </li>
+          </ul>
+        </article>
+        {/* end of Web Development */}
+
+        <article className="service">
+          <div className="service_head">
+            <h3>Content creation</h3>
+          </div>
+          <ul className="service_list">
+            <li>
+              <BsCheck2 className='service__list-icon'/>
+              <p>Lorem ipsum, dolor sit amet consectetur elit.</p>
+            </li>
+            <li>
+              <BsCheck2 className='service__list-icon'/>
+              <p>Lorem ipsum, dolor sit amet consectetur elit.</p>
+            </li>
+            <li>
+              <BsCheck2 className='service__list-icon'/>
+              <p>Lorem ipsum, dolor sit amet consectetur elit.</p>
+            </li>
+            <li>
+              <BsCheck2 className='service__list-icon'/>
+              <p>Lorem ipsum, dolor sit amet consectetur elit.</p>
+            </li>
+            <li>
+              <BsCheck2 className='service__list-icon'/>
+              <p>Lorem ipsum, dolor sit amet consectetur elit.</p>
+            </li>
+            <li>
+              <BsCheck2 className='service__list-icon'/>
+              <p>Lorem ipsum, dolor sit amet consectetur elit.</p>
+            </li>
+          </ul>
+        </article>
+        {/* end of first card */}
+      </div>
+    </section>
   )
 }
 

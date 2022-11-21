34576a8d42d5e4dbcdbb99938ab1cfc655eb9a0d
Nomawethu <nomawethu.Khoza@dadevs.co.za>
2022-11-21 16:17:18 +0200

    contact part

--- src/components/contacts/contact.jsx
+++ src/components/contacts/contact.jsx
@@ -1,10 +1,57 @@
-import React from 'react'
+import React, { useRef } from 'react';
 import './contact.css';
+import {MdEmail} from 'react-icons/md'
+import {FaWhatsappSquare} from 'react-icons/fa'
 
-const contact = () => {
+import emailjs from 'emailjs-com';
+
+
+
+
+const Contact = () => {
+
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', form.current, 'YOUR_PUBLIC_KEY')
+     
+  };
+
   return (
-    <section id="contact" >contact</section>
+    <section id="contact" >
+      <h5>Get in Touch</h5>
+      <h2>Contact me</h2>
+
+      <div className="container contact__container">
+        <div className="contact__options">
+            <article className="contact__option">
+            <MdEmail className='contact__option-icon'/>
+            <h4>Email</h4>
+            <h5>nomawehuk2@gmail.com</h5>
+            <a href="mailto:nomawehuk2@gmail.com" target="_blank">Send a message</a>
+            </article>
+
+            <article className="contact__option">
+            <FaWhatsappSquare className='contact__option-icon'/>
+            <h4>Whatsapp</h4>
+            <h5>0712345678</h5>
+            <a href="https://api.whatsapp.com/send?hone+27796543234" target="_blank">Send a message</a>
+            </article>
+        </div>
+        
+        {/* End of contact option */}
+
+        <form action="">
+            <input type="text" name='name' placeholder='Your full name' required/>
+            <input type="email" name='email' placeholder='Your Email' required/>
+            <textarea type="message" rows="7" placeholder='Your Message' required  />
+            <button type='submit' className='btn btn-primary'>Send Message</button>
+        </form>
+      </div>
+    </section>
   )
 }
 
-export default contact
+export default Contact
